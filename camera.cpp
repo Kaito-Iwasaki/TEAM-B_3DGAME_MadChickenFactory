641c67e82e09924db60e22e207173dace2c72826
Kaito-Iwasaki <251307@st.yoshida-g.ac.jp>
2026-01-20 12:03:25 +0900

    カメラ処理編集

--- camera.cpp
+++ camera.cpp
@@ -14,10 +14,21 @@
 #include "input.h"
 #include "DebugProc.h"
 #include "util.h"
+#include "player.h"
 
 //*********************************************************************
 // 
 // ***** マクロ定義 *****
+// 
+//*********************************************************************
+#define CAMERA_VECTOR_UP		D3DXVECTOR3(0, 1, 0)						// 上向きベクトル
+#define INIT_CAMERA_ROT			D3DXVECTOR3(D3DXToRadian(-25), 0, 0)		// カメラ回転初期値
+#define INIT_CAMERA_OFFSET		D3DXVECTOR3(0, 150, 0)						// カメラオフセット初期値
+#define INIT_CAMERA_DISTANCE	(1000)										// カメラ距離初期値
+
+//*********************************************************************
+// 
+// ***** 構造体 *****
 // 
 //*********************************************************************
 
@@ -25,13 +36,6 @@
 //*********************************************************************
 // 
 // ***** 列挙型 *****
-// 
-//*********************************************************************
-
-
-//*********************************************************************
-// 
-// ***** 構造体 *****
 // 
 //*********************************************************************
 
@@ -56,11 +60,10 @@ CAMERA g_camera[CAMERATYPE_MAX];
 void InitCamera(void)
 {
 	ZeroMemory(&g_camera[0], sizeof(g_camera));
-	g_camera[CAMERATYPE_GAME].posV = D3DXVECTOR3(0, 500, -400);
-	g_camera[CAMERATYPE_GAME].posR = D3DXVECTOR3(0, 0, 0);
-	g_camera[CAMERATYPE_GAME].vecU = D3DXVECTOR3(0, 1, 0);
-	g_camera[CAMERATYPE_GAME].rot = D3DXVECTOR3(D3DXToRadian(-55), D3DXToRadian(0), 0);
-	g_camera[CAMERATYPE_GAME].fDistance = 100.0f;
+	g_camera[CAMERATYPE_GAME].vecU = CAMERA_VECTOR_UP;
+	g_camera[CAMERATYPE_GAME].rot = INIT_CAMERA_ROT;
+	g_camera[CAMERATYPE_GAME].offset = INIT_CAMERA_OFFSET;
+	g_camera[CAMERATYPE_GAME].fDistance = INIT_CAMERA_DISTANCE;
 	g_camera[CAMERATYPE_GAME].viewport.X = 0;
 	g_camera[CAMERATYPE_GAME].viewport.Y = 0;
 	g_camera[CAMERATYPE_GAME].viewport.Width = SCREEN_WIDTH ;
@@ -83,11 +86,16 @@ void UninitCamera(void)
 void UpdateCamera(void)
 {
 	CAMERA* pCamera = &g_camera[0];
+	Player* pPlayer = GetPlayer();
 
-	// カメラ位置の設定
-	pCamera->posR.x = pCamera->posV.x + sinf(pCamera->rot.y) * cosf(pCamera->rot.x) * pCamera->fDistance;
-	pCamera->posR.y = pCamera->posV.y + sinf(pCamera->rot.x) * pCamera->fDistance;
-	pCamera->posR.z = pCamera->posV.z + cosf(pCamera->rot.y) * cosf(pCamera->rot.x) * pCamera->fDistance;
+	// カメラの注視点をプレイヤーに設定
+	SetCameraPosR(0, pPlayer->pos);
+
+	// カメラの視点を設定
+	SetCameraPosVFromAngle(0);
+
+	// カメラをオフセット分ずらす
+	MoveCamera(0, pCamera->offset);
 }
 
 //=====================================================================
@@ -132,9 +140,95 @@ void SetCamera(int nIdx)
 }
 
 //=====================================================================
-// カメラ処理
+// カメラ取得処理
 //=====================================================================
 CAMERA* GetCamera(int nIdx)
 {
 	return &g_camera[nIdx];
+}
+
+//=====================================================================
+// カメラ移動処理
+//=====================================================================
+void MoveCamera(int nIdx, D3DXVECTOR3 move)
+{
+	CAMERA* pCamera = &g_camera[nIdx];
+
+	// カメラの視点と注視点を同量動かす（平行移動）
+	pCamera->posV += move;
+	pCamera->posR += move;
+}
+
+//=====================================================================
+// 視点の位置設定処理
+//=====================================================================
+void SetCameraPosV(int nIdx, D3DXVECTOR3 pos)
+{
+	CAMERA* pCamera = &g_camera[nIdx];
+
+	pCamera->posV = pos;
+}
+
+//=====================================================================
+// 注視点の位置設定処理
+//=====================================================================
+void SetCameraPosR(int nIdx, D3DXVECTOR3 pos)
+{
+	CAMERA* pCamera = &g_camera[nIdx];
+
+	pCamera->posR = pos;
+}
+
+//=====================================================================
+// 角度から視点の位置を設定する処理
+//=====================================================================
+void SetCameraPosVFromAngle(int nIdx)
+{
+	CAMERA* pCamera = &g_camera[nIdx];
+
+	// 角度から視点の位置を求める
+	pCamera->posV.x = pCamera->posR.x - sinf(pCamera->rot.y) * cosf(pCamera->rot.x) * pCamera->fDistance;
+	pCamera->posV.z = pCamera->posR.z - cosf(pCamera->rot.y) * cosf(pCamera->rot.x) * pCamera->fDistance;
+	pCamera->posV.y = pCamera->posR.y - sinf(pCamera->rot.x) * pCamera->fDistance;
+}
+
+//=====================================================================
+// 角度から注視点の位置を設定する処理
+//=====================================================================
+void SetCameraPosRFromAngle(int nIdx)
+{
+	CAMERA* pCamera = &g_camera[nIdx];
+
+	// 角度から注視点の位置を求める
+	pCamera->posR.x = pCamera->posV.x + sinf(pCamera->rot.y) * cosf(pCamera->rot.x) * pCamera->fDistance;
+	pCamera->posR.z = pCamera->posV.z + cosf(pCamera->rot.y) * cosf(pCamera->rot.x) * pCamera->fDistance;
+	pCamera->posR.y = pCamera->posV.y + sinf(pCamera->rot.x) * pCamera->fDistance;
+}
+
+//=====================================================================
+// カメラ視点回転処理
+//=====================================================================
+void RotateCameraByV(int nIdx, D3DXVECTOR3 rotMove)
+{
+	CAMERA* pCamera = &g_camera[nIdx];
+
+	// カメラの角度を変更
+	pCamera->rot = GetFixedRotation(pCamera->rot + rotMove);
+
+	// 角度から注視点の位置を設定
+	SetCameraPosRFromAngle(nIdx);
+}
+
+//=====================================================================
+// カメラ注視点回転処理
+//=====================================================================
+void RotateCameraByR(int nIdx, D3DXVECTOR3 rotMove)
+{
+	CAMERA* pCamera = &g_camera[nIdx];
+
+	// カメラの角度を変更
+	pCamera->rot = GetFixedRotation(pCamera->rot + rotMove);
+
+	// 角度から視点の位置を設定
+	SetCameraPosVFromAngle(nIdx);
 }
--- camera.h
+++ camera.h
@@ -44,6 +44,7 @@ typedef struct CAMERA
 	D3DXVECTOR3 posR;
 	D3DXVECTOR3 vecU;
 	D3DXVECTOR3 rot;
+	D3DXVECTOR3 offset;
 	float fDistance;
 	D3DXMATRIX mtxProjection;	// プロジェクションマトリックス（投影行列）
 	D3DXMATRIX mtxView;			// ビューマトリックス
@@ -60,5 +61,12 @@ void UninitCamera(void);
 void UpdateCamera(void);
 void SetCamera(int nIdx);
 CAMERA* GetCamera(int nIdx);
+void MoveCamera(int nIdx, D3DXVECTOR3 move);
+void SetCameraPosV(int nIdx, D3DXVECTOR3 pos);
+void SetCameraPosR(int nIdx, D3DXVECTOR3 pos);
+void SetCameraPosVFromAngle(int nIdx);
+void SetCameraPosRFromAngle(int nIdx);
+void RotateCameraByV(int nIdx, D3DXVECTOR3 rotMove);
+void RotateCameraByR(int nIdx, D3DXVECTOR3 rotMove);
 
 #endif
