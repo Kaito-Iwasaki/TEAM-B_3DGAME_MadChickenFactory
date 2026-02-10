acdbbd7d5c22c6f4be8cc08a2630b4721e865cff
Airi Takahashi <251318@st.yoshida-g.ac.jp>
2026-02-10 10:07:51 +0900

    2P表示

--- player.cpp
+++ player.cpp
@@ -288,54 +288,58 @@ void DrawPlayer(void)
 	D3DXMATERIAL* pMat;								// マテリアルデータへのポインタ
 	Player* pPlayer = GetPlayer();
 
-	pDevice->GetMaterial(&matDef);
-
-	// プレイヤーのマトリックスを設定
-	MatrixRotationPosition(&pPlayer->mtxWorld, pPlayer->rot, pPlayer->pos);
-
-	for (int nCntPart = 0; nCntPart < pPlayer->PlayerMotion.nNumPart; nCntPart++)
+	for (int nCntPlayer = 0; nCntPlayer < MAX_PLAYER; nCntPlayer++, pPlayer++)
 	{
-		D3DXMATRIX mtxRotModel, mtxTransModel;		// 計算用マトリックス
-		D3DXMATRIX mtxParent;						// 親のマトリックス
+		pDevice->GetMaterial(&matDef);
 
-		// パーツへのポインタを取得
-		PART* pPart = &pPlayer->PlayerMotion.aPart[nCntPart];
+		// プレイヤーのマトリックスを設定
+		MatrixRotationPosition(&pPlayer->mtxWorld, pPlayer->rot, pPlayer->pos);
 
-		// パーツのワールドマトリックスの初期化
-		MatrixRotationPosition(&pPart->mtxWorld, pPart->rot, pPart->pos);
-
-		// パーツの「親マトリックス」を設定
-		if (pPart->nIdxModelParent != -1)
-		{// 親モデルがある場合
-			// 親のインデックスを使用して、同じプレイヤーのパーツ配列から取得
-			int parentIdx = pPart->nIdxModelParent;
-			mtxParent = pPlayer->PlayerMotion.aPart[parentIdx].mtxWorld;
-		}
-		else
-		{// 親モデルが無い場合
-			mtxParent = pPlayer->mtxWorld;
-		}
-
-		// 算出した「パーツのワールドマトリックス」と「親のマトリックス」をかけ合わせる
-		D3DXMatrixMultiply(&pPart->mtxWorld, &pPart->mtxWorld, &mtxParent);
-
-		// パーツのワールドマトリックスの設定
-		pDevice->SetTransform(D3DTS_WORLD, &pPart->mtxWorld);
-
-		// マテリアルデータへのポインタを取得
-		pMat = (D3DXMATERIAL*)pPart->pBuffMat->GetBufferPointer();
-
-		for (int nCntMat = 0; nCntMat < (int)pPart->dwNumMat; nCntMat++)
+		for (int nCntPart = 0; nCntPart < pPlayer->PlayerMotion.nNumPart; nCntPart++)
 		{
-			// マテリアルの設定
-			pDevice->SetMaterial(&pMat[nCntMat].MatD3D);
+			D3DXMATRIX mtxRotModel, mtxTransModel;		// 計算用マトリックス
+			D3DXMATRIX mtxParent;						// 親のマトリックス
 
-			// テクスチャ設定
-			pDevice->SetTexture(0, pPart->apTexture[nCntMat]);
+			// パーツへのポインタを取得
+			PART* pPart = &pPlayer->PlayerMotion.aPart[nCntPart];
 
-			// パーツの描画
-			pPart->pMesh->DrawSubset(nCntMat);
+			// パーツのワールドマトリックスの初期化
+			MatrixRotationPosition(&pPart->mtxWorld, pPart->rot, pPart->pos);
+
+			// パーツの「親マトリックス」を設定
+			if (pPart->nIdxModelParent != -1)
+			{// 親モデルがある場合
+				// 親のインデックスを使用して、同じプレイヤーのパーツ配列から取得
+				int parentIdx = pPart->nIdxModelParent;
+				mtxParent = pPlayer->PlayerMotion.aPart[parentIdx].mtxWorld;
+			}
+			else
+			{// 親モデルが無い場合
+				mtxParent = pPlayer->mtxWorld;
+			}
+
+			// 算出した「パーツのワールドマトリックス」と「親のマトリックス」をかけ合わせる
+			D3DXMatrixMultiply(&pPart->mtxWorld, &pPart->mtxWorld, &mtxParent);
+
+			// パーツのワールドマトリックスの設定
+			pDevice->SetTransform(D3DTS_WORLD, &pPart->mtxWorld);
+
+			// マテリアルデータへのポインタを取得
+			pMat = (D3DXMATERIAL*)pPart->pBuffMat->GetBufferPointer();
+
+			for (int nCntMat = 0; nCntMat < (int)pPart->dwNumMat; nCntMat++)
+			{
+				// マテリアルの設定
+				pDevice->SetMaterial(&pMat[nCntMat].MatD3D);
+
+				// テクスチャ設定
+				pDevice->SetTexture(0, pPart->apTexture[nCntMat]);
+
+				// パーツの描画
+				pPart->pMesh->DrawSubset(nCntMat);
+			}
 		}
+
 	}
 
 	// 保存していたマテリアルを戻す
