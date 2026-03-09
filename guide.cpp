7583b4c6d53d84643a65b7dff3f5b4a14dddd6e1
Kaito-Iwasaki <251307@st.yoshida-g.ac.jp>
2026-03-09 11:32:13 +0900

    ガイド表示

--- guide.cpp
+++ guide.cpp
@@ -11,14 +11,16 @@
 // 
 //*********************************************************************
 #include "Guide.h"
-#include "util.h"
+#include "team_logo.h"
 
 //*********************************************************************
 // 
 // ***** マクロ定義 *****
 // 
 //*********************************************************************
-
+#define GUIDE_SIZE			(150)
+#define GUIDE_POS_START		D3DXVECTOR3(80, SCREEN_HEIGHT - (GUIDE_SIZE * 0.5f + 10), 0)
+#define GUIDE_OFFSET_X		(GUIDE_SIZE * 0.5f + 20)
 
 //*********************************************************************
 // 
@@ -51,8 +53,9 @@ typedef enum
 // ***** グローバル変数 *****
 // 
 //*********************************************************************
-LPDIRECT3DVERTEXBUFFER9 g_aVtxBuffGuide = NULL;
+LPDIRECT3DVERTEXBUFFER9 g_pVtxBuffGuide = NULL;
 LPDIRECT3DTEXTURE9 g_apTexBuffGuide[GUIDETYPE_MAX] = {};
+GUIDE g_aGuide[GUIDETYPE_MAX] = {};
 
 const char* g_aFilenameGuideTexture[GUIDETYPE_MAX] = {
 	"data\\TEXTURE\\guide000.png",
@@ -65,14 +68,15 @@ const char* g_aFilenameGuideTexture[GUIDETYPE_MAX] = {
 void InitGuide(void)
 {
 	LPDIRECT3DDEVICE9 pDevice = GetDevice();
+	GUIDE* pGuide = &g_aGuide[0];
 
 	// 頂点バッファの生成
 	pDevice->CreateVertexBuffer(
-		sizeof(VERTEX_3D) * GUIDETYPE_MAX,
+		sizeof(VERTEX_2D) * 4 * GUIDETYPE_MAX,
 		D3DUSAGE_WRITEONLY,
-		FVF_VERTEX_3D,
+		FVF_VERTEX_2D,
 		D3DPOOL_MANAGED,
-		&g_aVtxBuffGuide,
+		&g_pVtxBuffGuide,
 		NULL
 	);
 
@@ -85,6 +89,15 @@ void InitGuide(void)
 			&g_apTexBuffGuide[nTexture]
 		);
 	}
+
+	// 構造体の初期化
+	ZeroMemory(&g_aGuide[0], sizeof g_aGuide);
+	for (int nGuide = 0; nGuide < GUIDETYPE_MAX; nGuide++, pGuide++)
+	{
+		pGuide->transform.pos = GUIDE_POS_START + D3DXVECTOR3(GUIDE_OFFSET_X * nGuide, 0, 0);
+		pGuide->transform.size = D3DXVECTOR3(GUIDE_SIZE, GUIDE_SIZE, 0);
+		pGuide->color = D3DXCOLOR_WHITE;
+	}
 }
 
 //=====================================================================
@@ -92,7 +105,14 @@ void InitGuide(void)
 //=====================================================================
 void UninitGuide(void)
 {
+	// 頂点バッファの破棄
+	RELEASE(g_pVtxBuffGuide);
 
+	// テクスチャの破棄
+	for (int nTexture = 0; nTexture < GUIDETYPE_MAX; nTexture++)
+	{
+		RELEASE(g_apTexBuffGuide[nTexture]);
+	}
 }
 
 //=====================================================================
@@ -108,5 +128,64 @@ void UpdateGuide(void)
 //=====================================================================
 void DrawGuide(void)
 {
+	// 1Pモード以外では必要ないので表示しない
+	if (GetTitle() != 0)
+	{
+		return;
+	}
 
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();
+
+	VERTEX_2D* pVtx;
+	GUIDE* pGuide = &g_aGuide[0];
+
+	g_pVtxBuffGuide->Lock(0, 0, (void**)&pVtx, 0);
+
+	for (int nGuide = 0; nGuide < GUIDETYPE_MAX; nGuide++, pGuide++)
+	{
+		pVtx[0].pos.x = pGuide->transform.pos.x - pGuide->transform.size.x * 0.5f;
+		pVtx[0].pos.y = pGuide->transform.pos.y - pGuide->transform.size.y * 0.5f;
+		pVtx[0].pos.z = 0.0f;
+
+		pVtx[1].pos.x = pGuide->transform.pos.x + pGuide->transform.size.x * 0.5f;
+		pVtx[1].pos.y = pGuide->transform.pos.y - pGuide->transform.size.y * 0.5f;
+		pVtx[1].pos.z = 0.0f;
+
+		pVtx[2].pos.x = pGuide->transform.pos.x - pGuide->transform.size.x * 0.5f;
+		pVtx[2].pos.y = pGuide->transform.pos.y + pGuide->transform.size.y * 0.5f;
+		pVtx[2].pos.z = 0.0f;
+
+		pVtx[3].pos.x = pGuide->transform.pos.x + pGuide->transform.size.x * 0.5f;
+		pVtx[3].pos.y = pGuide->transform.pos.y + pGuide->transform.size.y * 0.5f;
+		pVtx[3].pos.z = 0.0f;
+
+		pVtx[0].rhw = 1.0f;
+		pVtx[1].rhw = 1.0f;
+		pVtx[2].rhw = 1.0f;
+		pVtx[3].rhw = 1.0f;
+
+		pVtx[0].col = pGuide->color;
+		pVtx[1].col = pGuide->color;
+		pVtx[2].col = pGuide->color;
+		pVtx[3].col = pGuide->color;
+
+		pVtx[0].tex = D3DXVECTOR2(0.0f, 0.0f);
+		pVtx[1].tex = D3DXVECTOR2(1.0f, 0.0f);
+		pVtx[2].tex = D3DXVECTOR2(0.0f, 1.0f);
+		pVtx[3].tex = D3DXVECTOR2(1.0f, 1.0f);
+
+		pVtx += 4;
+	}
+
+	g_pVtxBuffGuide->Unlock();
+
+	pDevice->SetStreamSource(0, g_pVtxBuffGuide, 0, sizeof(VERTEX_2D));
+	pDevice->SetFVF(FVF_VERTEX_2D);
+
+	for (int nGuide = 0; nGuide < GUIDETYPE_MAX; nGuide++)
+	{
+		pDevice->SetTexture(0, g_apTexBuffGuide[nGuide]);
+
+		pDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP, nGuide * 4, 2);
+	}
 }
